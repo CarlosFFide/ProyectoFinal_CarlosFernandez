edbedbad200b4b898b538e1bd9f42e4dcd189a5b
Carlos Fernandez <cfernandez20742@ufide.ac.cr>
2026-03-15 19:17:35 -0600

    Parte 1 - configuracion inicial, Identity y base de datos

---
+++ ProyectoFinal_FernandezCarlos/Models/Carreras.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal_FernandezCarlos.Models
+{
+    public class Carrera
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Codigo { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        public ICollection<Curso> Cursos { get; set; } = new List<Curso>();
+        public ICollection<Estudiante> Estudiantes { get; set; } = new List<Estudiante>();
+    }
+}
---
+++ ProyectoFinal_FernandezCarlos/Models/Matricula.cs
@@ -0,0 +1,15 @@
+﻿namespace ProyectoFinal_FernandezCarlos.Models
+{
+    public class Matricula
+    {
+        public int Id { get; set; }
+
+        public int EstudianteId { get; set; }
+        public Estudiante? Estudiante { get; set; }
+
+        public int CursoId { get; set; }
+        public Curso? Curso { get; set; }
+
+        public DateTime FechaMatricula { get; set; } = DateTime.Now;
+    }
+}
